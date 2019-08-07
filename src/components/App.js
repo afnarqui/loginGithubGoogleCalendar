4bf63cf47d9667ada20f1b1ef4ac67d8bf5d6f01
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-08-07 14:35:50 -0500

    CU4: create components Login

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import {Login} from './Login'
 
-const App = () => {
-  return (
-    <div>
-      initial
-    </div>
-  )
-}
+const App = () => (
+  <Login />
+)
 
 export default App
