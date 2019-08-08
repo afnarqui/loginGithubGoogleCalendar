9c254d2bb8c481d6db9eb6211105e5022c65f7aa
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-08-07 19:50:15 -0500

    CU8: implement proteted routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { Home } from './pages/Home'
+import { User } from './pages/User'
+import { Register } from './pages/Register'
+import { GlobalStyles } from './styles/GlobalStyles'
+
 import { NavBar } from './components/NavBar'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
     <div>
+      <GlobalStyles />
       <Router>
         <Home path='/' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <User path='/' />
+              </Router>
+              : <Router>
+                <Register path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Login } from '../components/Login'
-import { GlobalStyle } from '../styles/GlobalStyles'
 
 export const Home = () => {
   return (
     <div>
-      <GlobalStyle />
       <Login />
     </div>
   )
