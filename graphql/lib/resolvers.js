932804a4e39f2f1f96d4875a73ed5a37cefb33a5
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-08-10 03:55:57 -0500

    CU15: create arguments in query Users

--- graphql/lib/resolvers.js
+++ graphql/lib/resolvers.js
@@ -9,7 +9,13 @@ const users = [
 ]
 
 module.exports = {
+  Query: {
   getUsers: () => {
     return users
+  },
+  getUser: (root, args) => {
+    const user = users.filter(user => user._id === args.id)
+    return user.pop()
   }
+}
 }
