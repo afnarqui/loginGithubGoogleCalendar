68812a82a3262a678d59fbaf81fb4a96aac44b4c
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-08-10 03:02:28 -0500

    CU10: create api graphql

---
+++ graphql/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { graphql, buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+graphql(schema, '{ hello }')
+  .then(( data ) => {
+    console.log(data)
+  })
+
+
