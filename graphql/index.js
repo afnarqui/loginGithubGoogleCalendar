48615d48fa30c611f8eb67e4de4ab44218312c5f
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-08-10 03:24:52 -0500

    CU12: create custom types schema

--- graphql/index.js
+++ graphql/index.js
@@ -3,14 +3,18 @@
 const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
 const app = express()
 const port = process.env.port || 3000
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+const schema = buildSchema(
+  readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+  )
+  )
 
 const resolvers = {
   hello: () => {
